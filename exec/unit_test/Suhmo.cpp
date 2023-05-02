4c6c72672f572e47a4651b7c7b6c8196cb57a343
Enna Delfen <fanne.af@gmail.com>
2023-05-02 15:53:54 -0700

    Add the previously mentioned unit_test test case BUT note that the Sqrt IBC needs to be adapted, otherwise it's just 1_convergence_distributed/128x32 . I will automatize at some point

---
+++ exec/unit_test/Suhmo.cpp
@@ -0,0 +1,165 @@
+#ifdef CH_LANG_CC
+/*
+*      _______              __
+*     / ___/ /  ___  __ _  / /  ___
+*    / /__/ _ \/ _ \/  V \/ _ \/ _ \
+*    \___/_//_/\___/_/_/_/_.__/\___/
+*    Please refer to Copyright.txt, in Chombo's root directory.
+*/
+#endif
+
+//===========================================================================
+// driver.cpp
+//
+//===========================================================================
+#include <iostream>
+#include <fstream>
+#include "ParmParse.H"
+#include "AMRIO.H"
+#include "SPMD.H"
+
+#include "AmrHydro.H"
+
+#include "HydroIBC.H"
+#include "SqrtIBC.H"
+#include "ValleyIBC.H"
+#include "MountainSetupIBC.H"
+//#include "FileIBC.H"
+#include "memusage.H" 
+
+#ifdef CH_USE_PETSC
+#include "petsc.h"
+#endif
+
+//===========================================================================
+// 2D Amr driver example
+//
+//===========================================================================
+int
+main(int argc, char* argv[])
+{
+    int ierr = 0;
+
+#ifdef CH_USE_PETSC
+    ierr = PetscInitialize(&argc, &argv, PETSC_NULL, PETSC_NULL);
+    CHKERRQ(ierr);
+#else
+#ifdef CH_MPI
+    MPI_Init(&argc, &argv);
+#endif
+#endif // end petsc conditional
+
+    { // Begin nested scope
+
+#ifdef CH_MPI
+        MPI_Barrier(Chombo_MPI::comm);
+#endif
+        int rank, number_procs;
+#ifdef CH_MPI
+        MPI_Comm_rank(Chombo_MPI::comm, &rank);
+        MPI_Comm_size(Chombo_MPI::comm, &number_procs);
+#else
+        rank = 0;
+        number_procs = 1;
+#endif
+
+        if (argc < 2)
+        {
+            std::cerr << " usage: " << argv[0] << " <input_file>\n";
+            exit(0);
+        }
+        char* in_file = argv[1];
+        ParmParse pp(argc - 2, argv + 2, NULL, in_file);
+        ParmParse ppMain("main");
+
+        std::string poutBaseName = "pout";
+        ppMain.query("poutBaseName", poutBaseName);
+        setPoutBaseName(poutBaseName);
+
+        RealVect domainSize;
+        Vector<Real> domSize(SpaceDim);
+        ppMain.getarr("domain_size", domSize, 0, SpaceDim);
+        domainSize = RealVect(D_DECL(domSize[0], domSize[1], domSize[2]));
+
+        AmrHydro amrObject;
+
+        HydroIBC* hydroIBCPtr = NULL;
+
+        // ---------------------------------------------
+        // set IBC -- this includes initial conditon
+        // and boundary conditions
+        // ---------------------------------------------
+        std::string problem_type;
+        ppMain.get("problem_type", problem_type);
+
+        if (problem_type == "basic") {
+            hydroIBCPtr = new HydroIBC();
+        } else if (problem_type == "sqrt") {
+            SqrtIBC* ibcPtr = new SqrtIBC;
+            hydroIBCPtr = static_cast<HydroIBC*>(ibcPtr);
+        } else if (problem_type == "valley") {
+            ValleyIBC* ibcPtr = new ValleyIBC;
+            ParmParse vPP("valleypp");
+            Real gamma;
+            vPP.get("gamma", gamma);
+            ibcPtr->setParameters(gamma); 
+            hydroIBCPtr = static_cast<HydroIBC*>(ibcPtr);
+        } else if (problem_type == "dino") {
+            MountainIBC* ibcPtr = new MountainIBC;
+            hydroIBCPtr = static_cast<HydroIBC*>(ibcPtr);
+        //} else if (problem_type == "file") {
+        //  FileIBC* ibcPtr = new FileIBC;
+        //  ParmParse fpp("fileIBC");
+        //  std::string geomFile;
+        //  fpp.get("geometryFile", geomFile);
+        //  std::string thicknessName = "thck";
+        //  fpp.query("thicknessName", thicknessName);
+        //  std::string topographyName = "topg";
+        //  fpp.query("topographyName",topographyName);          
+        //  std::string velXFile;
+        //  fpp.get("velXFile", velXFile);
+        //  std::string velYFile;
+        //  fpp.get("velYFile", velYFile);
+        //  ibcPtr->setup(geomFile, thicknessName, topographyName, velXFile, velYFile);
+        //  hydroIBCPtr = static_cast<HydroIBC*>(ibcPtr);          
+        } else {
+            MayDay::Error("bad problem_type !");
+        }
+
+        amrObject.setIBC(hydroIBCPtr);
+
+        amrObject.setDomainSize(domainSize);
+
+        // set up initial grids, initialize data, etc.
+        amrObject.initialize();
+
+        int maxStep;
+        Real maxTime;
+        // Real startTime;
+        ppMain.get("maxTime", maxTime);
+        ppMain.get("maxStep", maxStep);
+
+        amrObject.run(maxTime, maxStep);
+
+        if (hydroIBCPtr != NULL)
+        {
+            delete hydroIBCPtr;
+            hydroIBCPtr = NULL;
+        }
+
+    } // end nested scope
+
+    CH_TIMER_REPORT();
+    dumpmemoryatexit();
+
+#ifdef CH_USE_PETSC
+    ierr = PetscFinalize();
+    CHKERRQ(ierr);
+#else
+#ifdef CH_MPI
+    MPI_Finalize();
+#endif // mpi conditional
+#endif // petsc conditional
+
+    return ierr;
+}
